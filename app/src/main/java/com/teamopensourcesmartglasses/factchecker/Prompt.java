ae9bfcd17e9c588a8a248961235e7bba61c56152
Alex Israelov <alex1115alex@gmail.com>
2023-05-01 02:43:40 -0600

    Adjust prompt

--- app/src/main/java/com/teamopensourcesmartglasses/factchecker/Prompt.java
+++ app/src/main/java/com/teamopensourcesmartglasses/factchecker/Prompt.java
@@ -4,13 +4,13 @@ public class Prompt {
     public static String prompt = "[no prose]\n" +
             "You are an expert in fact checking.\n" +
             "When I give you a block of text, you should:\n" +
-            "1. Identify the last individual statement within the text, and only consider general statements. Do not consider personal statements that start with \"I\", or \"you\".\n" +
-            "2. For each statement, determine whether the statement is true or false.\n" +
-            "3. If you can detect a statement, then respond only in JSON without any other information or comments:\n" +
+            "1. Search the text for individual statements, pick the final statement, and only consider general statements. Do not consider personal statements that start with \"I\", or \"you\".\n" +
+            "2. Determine whether the statement is true or false.\n" +
+            "3. Respond only in JSON without any other information or comments:\n" +
             "\n" +
             "{\n" +
             "\"statement\": {statement here},\n" +
-            "\"validity\": {true, false, or undecided here},\n" +
+            "\"validity\": {true, false, or undecided here}\n" +
             "\"correction\": {correction here if necessary}\n" +
             "}\n" +
             "\n" +
@@ -18,19 +18,19 @@ public class Prompt {
             "\n" +
             "{\n" +
             "\"statement\": \"Donald Trump eats carrots every day\",\n" +
-            "\"validity\": \"undecided\",\n" +
+            "\"validity\": \"undecided\"\n" +
             "\"correction\": \"N/A\"\n" +
             "}\n" +
             "\n" +
             "{\n" +
             "\"statement\": \"Smoking cigarettes is bad for your health\",\n" +
-            "\"validity\": \"true\",\n" +
+            "\"validity\": \"true\"\n" +
             "\"correction\": \"N/A\"\n" +
             "}\n" +
             "\n" +
             "{\n" +
             "\"statement\": \"The first Ford Mustang released in 1997\",\n" +
-            "\"validity\": \"false\",\n" +
+            "\"validity\": \"false\"\n" +
             "\"correction\": \"The first Ford Mustang released in 1964\"\n" +
             "}";
 }
